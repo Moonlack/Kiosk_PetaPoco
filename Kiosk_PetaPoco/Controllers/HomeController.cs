322fd7f06aac79cafa1fd9a887dfa26ef770216b
sashaavramchik <sashaavramchik@live.com>
2017-06-22 01:09:02 +0300

    Создание проекта

---
+++ Kiosk_PetaPoco/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kiosk_PetaPoco.Models;
+using PetaPoco;
+
+namespace Kiosk_PetaPoco.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var db = new Database("Infokiosk");
+            var a = new Exhibit { Name = "Srani poko", Description = "SUUUKA", IsMedal = false };
+            db.Insert(a);
+            var b = db.Query<Exhibit>("SELECT * FROM articles");
+                        return View(b);
+        }
+    }
+}
---
+++ Kiosk_PetaPoco/Models/Exhibit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PetaPoco;
+
+namespace Kiosk_PetaPoco.Models
+{
+    [TableName("Exhibits")]
+    [PrimaryKey("Exhibit_id")]
+    public class Exhibit
+    {
+        public long Exhibit_id { get; set; }
+        public string Name { get; set; }
+        public bool IsMedal { get; set; }
+        public string Description { get; set; }
+        public string Images { get; set; }
+    }
+}
